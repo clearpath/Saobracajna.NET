98d8299d3478340006a0ece9c4dcba9e27f7255f
Vladimir Ivankovic <clearpath@users.noreply.github.com>
2015-07-06 10:27:29 +0200

    win32 dll dodat u sample aplikaciju

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Saobracajna.NET je .NET wrapper za eVehicleRegistrationAPI biblioteku MUP-a Repu
 Nastao je da bi se izbegao rad sa COM komponentom, kako je inace predvidjeno da se koristi iz .NET-a, sto olaksava kasniji deployment aplikacije koja iscitava dozvole.
 
 Win32 .dll i prateci fajlovi su preuzeti sa sajta MUP-a u julu 2015.
+
+Krajnjem korisniku je potrebno isporuciti pored .NET wrapper-a i eVehicleRegistrationAPI.dll tj obezbediti da se eVehicleRegistrationAPI.dll nalazi pored .exe fajla.
--- src/Saobracajna.NET/Native/NativeMethods.cs
+++ src/Saobracajna.NET/Native/NativeMethods.cs
@@ -2,14 +2,16 @@
 {
 	static class NativeMethods
 	{
+		private const string DLL = "eVehicleRegistrationAPI.dll";
+
 		/// Return Type: int
 		///apiVersion: int
-		[System.Runtime.InteropServices.DllImportAttribute("<Unknown>", EntryPoint = "sdStartup")]
+		[System.Runtime.InteropServices.DllImportAttribute(DLL, EntryPoint = "sdStartup")]
 		public static extern int sdStartup(int apiVersion);
 
 
 		/// Return Type: int
-		[System.Runtime.InteropServices.DllImportAttribute("<Unknown>", EntryPoint = "sdCleanup")]
+		[System.Runtime.InteropServices.DllImportAttribute(DLL, EntryPoint = "sdCleanup")]
 		public static extern int sdCleanup();
 
 
@@ -17,43 +19,43 @@
 		///index: int
 		///readerName: char*
 		///nameSize: int*
-		[System.Runtime.InteropServices.DllImportAttribute("<Unknown>", EntryPoint = "GetReaderName")]
+		[System.Runtime.InteropServices.DllImportAttribute(DLL, EntryPoint = "GetReaderName")]
 		public static extern int GetReaderName(int index, System.IntPtr readerName, ref int nameSize);
 
 
 		/// Return Type: int
 		///readerName: char*
-		[System.Runtime.InteropServices.DllImportAttribute("<Unknown>", EntryPoint = "SelectReader")]
+		[System.Runtime.InteropServices.DllImportAttribute(DLL, EntryPoint = "SelectReader")]
 		public static extern int SelectReader(System.IntPtr readerName);
 
 
 		/// Return Type: int
-		[System.Runtime.InteropServices.DllImportAttribute("<Unknown>", EntryPoint = "sdProcessNewCard")]
+		[System.Runtime.InteropServices.DllImportAttribute(DLL, EntryPoint = "sdProcessNewCard")]
 		public static extern int sdProcessNewCard();
 
 
 		/// Return Type: int
 		///param0: SD_REGISTRATION_DATA*
 		///index: int
-		[System.Runtime.InteropServices.DllImportAttribute("<Unknown>", EntryPoint = "sdReadRegistration")]
+		[System.Runtime.InteropServices.DllImportAttribute(DLL, EntryPoint = "sdReadRegistration")]
 		public static extern int sdReadRegistration(ref groupSD_REGISTRATION_DATA param0, int index);
 
 
 		/// Return Type: int
 		///param0: SD_DOCUMENT_DATA*
-		[System.Runtime.InteropServices.DllImportAttribute("<Unknown>", EntryPoint = "sdReadDocumentData")]
+		[System.Runtime.InteropServices.DllImportAttribute(DLL, EntryPoint = "sdReadDocumentData")]
 		public static extern int sdReadDocumentData(ref groupSD_DOCUMENT_DATA param0);
 
 
 		/// Return Type: int
 		///param0: SD_VEHICLE_DATA*
-		[System.Runtime.InteropServices.DllImportAttribute("<Unknown>", EntryPoint = "sdReadVehicleData")]
+		[System.Runtime.InteropServices.DllImportAttribute(DLL, EntryPoint = "sdReadVehicleData")]
 		public static extern int sdReadVehicleData(ref groupSD_VEHICLE_DATA param0);
 
 
 		/// Return Type: int
 		///param0: SD_PERSONAL_DATA*
-		[System.Runtime.InteropServices.DllImportAttribute("<Unknown>", EntryPoint = "sdReadPersonalData")]
+		[System.Runtime.InteropServices.DllImportAttribute(DLL, EntryPoint = "sdReadPersonalData")]
 		public static extern int sdReadPersonalData(ref groupSD_PERSONAL_DATA param0);
 	}
 
